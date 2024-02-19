bb4e00e48512fd284e13b1bdd1bcc0b08a27c9f6
ethandoes-code <ethandoescode@gmail.com>
2024-02-19 08:45:34 -0600

    Add to test and refactor hello.go

--- hello.go
+++ hello.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
-const prefix string = "Hello "
+const (
+     engPrefix string = "Hello "
+     spanPrefix string = "Hola "
+     frenPrefix string = "Bonjour "
+     dudePrefix string = "Wassssup "    
+     spanish string = "Spanish"
+     french string = "French"
+     dude string = "Dude"
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
     if name == "" {
         name = "world"
-    }
+    } 
+    return greetingPrefix(language) + name + "!"
+}
 
-    return prefix + name + "!"
+func greetingPrefix(language string) (prefix string) { 
+    switch language {
+    case spanish:
+        prefix = spanPrefix
+    case french:
+        prefix = frenPrefix        
+    case dude:
+        prefix = dudePrefix
+    default:
+        prefix = engPrefix
+    }
+    return 
 }
 
 func main() {
-    fmt.Println(Hello("Chris"))
+    fmt.Println(Hello("Chris", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,32 @@ import "testing"
 
 func TestHello(t *testing.T) {
     t.Run("saying hello to people", func(t *testing.T) {
-        got := Hello("Chris")
+        got := Hello("Chris", "")
         want := "Hello Chris!"
         assertCorrectMessage(t, got, want)
     })
     
     t.Run("saying 'Hello world!' when an empty string is passed", func(t *testing.T) {
-        got := Hello("")
+        got := Hello("", "")
         want := "Hello world!"
+        assertCorrectMessage(t, got, want)
+    })
+    
+    t.Run("in Spanish", func(t *testing.T) {
+        got := Hello("Elodie", "Spanish")
+        want := "Hola Elodie!"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in French", func(t *testing.T) {
+        got := Hello("Jaque", "French")
+        want := "Bonjour Jaque!"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Dude", func(t *testing.T) {
+        got := Hello("Brad", "Dude")
+        want := "Wassssup Brad!"
         assertCorrectMessage(t, got, want)
     })
 }
