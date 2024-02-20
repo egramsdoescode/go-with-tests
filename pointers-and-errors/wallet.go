f315ea5707aa12e1737161d456c783cef1821e81
ethandoes-code <ethandoescode@gmail.com>
2024-02-20 07:55:49 -0600

    Continuing pointers and errors

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,15 +1,34 @@
 package main
 
+import (
+    "fmt"
+    "errors"
+)
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+    return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-     
+    balance Bitcoin 
 }
 
-func (w Wallet) Deposit(amount int) {
-
+func (w *Wallet) Deposit(amount Bitcoin) {
+    w.balance += amount
 }
 
-func (w Wallet) Balance() int {
-    return 0
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+    if amount > w.balance {
+        return errors.New("oh no")
+    }
+
+    w.balance -= amount
+    return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+    return w.balance
 }   
 
 
