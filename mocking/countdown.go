7a07eb0ae03a1b5c483d6c8389c6b69c17606fd6
ethandoes-code <ethandoescode@gmail.com>
2024-02-25 14:16:06 -0600

    Add subtest that checks sleep before every print

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,43 +1,57 @@
 package main
+
 import (
-    "fmt"
-    "io"
-    "os"
-    "time"
+	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
 const (
-    finalWord = "Go!"
-    countdownStart = 3
+	finalWord      = "Go!"
+	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
 )
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
 type Sleeper interface {
-    Sleep()
+	Sleep()
 }
-
 type SpySleeper struct {
-    Calls int
+	Calls int
 }
 
-type DefaultSleeper struct {}
+type DefaultSleeper struct{}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, sleep)
+	return
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
 
 func (d *DefaultSleeper) Sleep() {
-    time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second)
 }
 
 func (s *SpySleeper) Sleep() {
-    s.Calls++
+	s.Calls++
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-    for i := countdownStart; i > 0; i-- {
-        fmt.Fprintln(out, i)
-        sleeper.Sleep()
-    }
-    fmt.Fprint(out, finalWord)
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-    sleeper := &DefaultSleeper{}
-    Countdown(os.Stdout, sleeper)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
