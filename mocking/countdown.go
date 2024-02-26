4f9223409253ebcc42de6b6c45ebdd9613170622
ethandoes-code <ethandoescode@gmail.com>
2024-02-26 08:43:07 -0600

    Complete mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,68 +7,37 @@ import (
 	"time"
 )
 
-const (
-	finalWord      = "Go!"
-	countdownStart = 3
-	write          = "write"
-	sleep          = "sleep"
-)
-
-type SpyCountdownOperations struct {
-	Calls []string
-}
+// Sleeper allows you to put delays.
 type Sleeper interface {
 	Sleep()
 }
-type SpySleeper struct {
-	Calls int
-}
 
+// ConfigurableSleeper is an implementation of Sleeper with a defined delay.
 type ConfigurableSleeper struct {
 	duration time.Duration
 	sleep    func(time.Duration)
 }
 
-type SpyTime struct {
-	durationSlept time.Duration
-}
-
-type DefaultSleeper struct{}
-
+// Sleep will pause execution for the defined Duration.
 func (c *ConfigurableSleeper) Sleep() {
 	c.sleep(c.duration)
 }
 
-func (s *SpyTime) Sleep(duration time.Duration) {
-	s.durationSlept = duration
-}
+const finalWord = "Go!"
+const countdownStart = 3
 
-func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, sleep)
-	return
-}
-
-func (s *SpyCountdownOperations) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
+// Countdown prints a countdown from 3 to out with a delay between count provided by Sleeper.
 func Countdown(out io.Writer, sleeper Sleeper) {
+
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestCountdown(t *testing.T) {
+
 	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
 		Countdown(buffer, &SpyCountdownOperations{})
@@ -23,7 +24,6 @@ Go!`
 		}
 	})
 
-	// TODO: Fix this subtest!!
 	t.Run("sleep before every print", func(t *testing.T) {
 		spySleepPrinter := &SpyCountdownOperations{}
 		Countdown(spySleepPrinter, spySleepPrinter)
@@ -54,5 +54,28 @@ func TestConfigurableSleeper(t *testing.T) {
 	if spyTime.durationSlept != sleepTime {
 		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
+}
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
