3073d29f9c0e3a5a04de74bd00b0f8ab50df767d
ethandoes-code <ethandoescode@gmail.com>
2024-03-02 15:22:38 -0600

    Continue reflection

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -2,21 +2,76 @@ package main
 
 import (
     "testing"
+    "reflect"
 )
 
+type Person struct {
+    Name string
+    Profile Profile
+}
+
+type Profile struct {
+    Age int
+    City string
+}
+
 func TestWalk(t *testing.T) {
-    expected := "Chris"
-    var got []string
-
-    x := struct {
+    cases := []struct {
         Name string
-    }{expected}
+        Input interface{}
+        ExpectedCalls []string
+    }{
+        {
+            "struct with one string field",
+            struct {
+                Name string
+            }{"Chris"},
+            []string{"Chris"},
+        },
+        {
+            "struct with two string fields",
+            struct {
+                Name string
+                City string
+            }{"Chris", "London"},
+            []string{"Chris", "London"},
+        },
+        {
+            "struct with non string field",
+            struct {
+                Name string
+                Age  int
+            }{"Chris", 33},
+            []string{"Chris"},
+        },
+        {
+            "nested fields",
+            Person{
+                "Chris",
+                Profile{33, "London"},
+            },
+            []string{"Chris", "London"},
+        },
+        {
+            "pointers to things",
+            &Person{
+                "Chris",
+                Profile{33, "London"},
+            },
+            []string{"Chris", "London"},
+        },
+     }
 
-    walk(x, func(input string) {
-        got = append(got, input)
-    })
+    for _, test := range cases {
+        t.Run(test.Name, func(t *testing.T) {
+            var got []string
+            walk(test.Input, func(input string) {
+                got = append(got, input)
+            })
 
-    if len(got) != 1 {
-        t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+            if !reflect.DeepEqual(got, test.ExpectedCalls) {
+                t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+            }
+        })
     }
 }
