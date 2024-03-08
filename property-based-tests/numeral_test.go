29705e9ca25d86ceffcae1ca66573b6984426d4f
ethandoes-code <ethandoescode@gmail.com>
2024-03-07 23:08:34 -0600

    Add ConvertToArabic function

--- property-based-tests/numeral_test.go
+++ property-based-tests/numeral_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
+var (
+	cases = []struct {
 		Arabic int
 		Roman  string
 	}{
@@ -39,7 +39,10 @@ func TestRomanNumerals(t *testing.T) {
 		{Arabic: 2014, Roman: "MMXIV"},
 		{Arabic: 1006, Roman: "MVI"},
 		{Arabic: 798, Roman: "DCCXCVIII"},
-	}
+	} 
+)
+
+func TestRomanNumerals(t *testing.T) {
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
@@ -48,4 +51,15 @@ func TestRomanNumerals(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+    for _, test := range cases[:1] {
+        t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+            got := ConvertToArabic(test.Roman)
+            if got != test.Arabic {
+                t.Errorf("got %d, want %d", got, test.Arabic)
+            }
+        })
+    }
 }
